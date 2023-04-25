2e05fd2c0bc237189cc594bfcaed5ea5a43a4f19
AyimWilliam <odhisayim@gmail.com>
2023-04-25 17:35:35 +0300

    styles implemented

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,21 +1,44 @@
 import React, {useState, useEffect } from 'react'
 import TransactionList from './TransactionList'
+import TransactionForm from './TransactionForm';
 
 function App() {
 
     const [transactions, setTransactions] = useState([]);
+    const [searchWord, setSearchWord] = useState('');
+
+    //Fetching from the db.json happens here
     
     useEffect(() => {
         fetch('http://localhost:3000/transactions')
         .then (response => response.json())
         .then(transactions => setTransactions(transactions));
         }, []);
+
+    //Updating state to add a new transaction
+
+    const handleAddTransaction = newTransaction =>{
+      setTransactions([...transactions, newTransaction]);
+    };
+
+    //Handler for search
+
+    const handleSearch = word => {
+      setSearchWord(word)
+    };
+
+    const filteredTransactions = transactions.filter(transaction => {
+      return transaction.description.toLowerCase().includes(searchWord.toLowerCase());
+    });
     
   return (
     <div>
-        <TransactionList transactions={transactions}/>
+        <h1>Transactions</h1>
+        <TransactionForm onAddTransaction={handleAddTransaction} />
+        <SearchBar onSearch={handleSearch} />
+        <TransactionList transactions={filteredTransactions} />
     </div>
-  )
-}
+  );
+};
 
 export default App;
