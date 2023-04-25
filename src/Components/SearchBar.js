768c177ba642d6834240055e71a0fb748a0ee701
AyimWilliam <odhisayim@gmail.com>
2023-04-25 17:59:43 +0300

    Completed assignment

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -3,6 +3,7 @@ import React from 'react'
 function SearchBar({onSearch}) {
   return (
     <input
+    className='input'
     type='text'
     placeholder='Filter by description'
     onChange={e => onSearch(e.target.value)}
